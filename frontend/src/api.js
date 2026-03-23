2f832e8585e1d4fbc8e50b4b8b8fb52ee7d0ac2c
pcun89 <phillippcun89@gmail.com>
2026-03-22 20:49:31 -0500

    fix api.js i forgot to put a /

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://expense-tracker-backend-204916618868.us-central1.run.appapi"
+    baseURL: "https://expense-tracker-backend-204916618868.us-central1.run.app/api"
 });
